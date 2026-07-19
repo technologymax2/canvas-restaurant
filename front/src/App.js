53e6fc090c26f610d8832c66f58df4585a9c030d
technologymax2 <technologymax2@gmail.com>
2026-07-19 02:51:41 -0700

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,7 @@ import Login from './components/Login';
 import AdminDashboard from './components/AdminDashboard';
 import OrderPage from './components/OrderPage';
 import Footer from './components/Footer';
-import logoImg from './CanvasLogo.png';
+import logoImg from './CanvasLogo2.png';
 import './App.css';
 
 function App() {
